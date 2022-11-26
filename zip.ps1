55537a5694e4c8a82481f6d6e929b1e66bb60669
AlexGiazitzis <a.giazitzis@outlook.com>
2022-11-26 18:36:05 +0200

    Fix zipping script.

--- zip.ps1
+++ zip.ps1
@@ -1,4 +1,4 @@
-Remove-Item 
+Remove-Item '.\Introduction to Python (EN).zip', '.\Introduction to Python (GR).zip'
 Compress-Archive -Path '.\Introduction to Python (EN).pdf', '.\code examples' -DestinationPath '.\Introduction to Python (EN).zip'
 Compress-Archive -Path '.\Introduction to Python (GR).pdf', '.\code examples' -DestinationPath '.\Introduction to Python (GR).zip'
 Remove-Item '.\Introduction to Python (EN).pdf', '.\Introduction to Python (GR).pdf'
